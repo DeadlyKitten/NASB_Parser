ca41c211225cef78f0b9f4aed09abe86511f7321
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2022-11-13 01:18:17 -0800

    Add FSLastAtk

---
+++ NASB_Parser/FloatSources/FSLastAtk.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NASB_Parser.FloatSources
+{
+    public class FSLastAtk : FloatSource
+    {
+		public Attributes Attribute { get; set; }
+
+		public FSLastAtk()
+		{
+		}
+
+		internal FSLastAtk(BulkSerializeReader reader) : base(reader)
+		{
+			Attribute = (Attributes)reader.ReadInt();
+		}
+
+		public override void Write(BulkSerializeWriter writer)
+		{
+			base.Write(writer);
+			writer.Write(Attribute);
+		}
+
+		public enum Attributes
+		{
+			Type,
+			DamageBase,
+			DamageMult,
+			KnockbackMult,
+			DamageTotal,
+			Angle,
+			Direction,
+			DiType,
+			DiIn,
+			DiOut,
+			Reversible,
+			KnockbackType,
+			KnockbackBase,
+			KnockbackGain,
+			ExtraKbAboveKb,
+			ExtraKbMult,
+			StunCalc,
+			StunBase,
+			StunGain,
+			HitOpponent,
+			InteractDirection,
+			Blockstun,
+			Blockpush,
+			Blocklag,
+			Hitlag,
+			HitlagSelf,
+			Launcher,
+			LaunchAboveKb,
+			LaunchArmorLevel,
+			ForceJabReset,
+			Grablevel,
+			Grabtype,
+			Killshot,
+			Directionalfx,
+			Unblockable,
+			Pierceinvincible,
+			NoBounce
+		}
+	}
+}
