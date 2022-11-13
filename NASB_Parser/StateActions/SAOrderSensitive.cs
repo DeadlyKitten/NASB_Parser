c4b36d9d2dc472f812e1943b0229fab8a56d5633
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2022-11-13 01:22:01 -0800

    Rename SAOrderSensitive

--- NASB_Parser/StateActions/SAOrderSensitive.cs
+++ NASB_Parser/StateActions/SAOrderSensitive.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace NASB_Parser.StateActions
 {
-    public class SAOrderedSensitive : StateAction
+    public class SAOrderSensitive : StateAction
     {
         public List<StateAction> Actions { get; private set; } = new List<StateAction>();
 
-        public SAOrderedSensitive()
+        public SAOrderSensitive()
         {
         }
 
-        internal SAOrderedSensitive(BulkSerializeReader reader) : base(reader)
+        internal SAOrderSensitive(BulkSerializeReader reader) : base(reader)
         {
             Actions = reader.ReadList(r => Read(r));
         }
