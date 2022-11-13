c33bdd0b148c422744ccf635b62e087fb3fc57c9
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2022-11-13 01:22:13 -0800

    Add SAPersistLocalFX

---
+++ NASB_Parser/StateActions/SAPersistLocalFX.cs
@@ -0,0 +1,30 @@
+﻿using NASB_Parser.FloatSources;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NASB_Parser.StateActions
+{
+    public class SAPersistLocalFX : StateAction
+    {
+        public FloatSource Persist { get; set; }
+        public bool MaxOut { get; set; }
+
+        public SAPersistLocalFX()
+        {
+        }
+
+        internal SAPersistLocalFX(BulkSerializeReader reader) : base(reader)
+        {
+            Persist = FloatSource.Read(reader);
+            MaxOut = reader.ReadBool();
+        }
+
+        public override void Write(BulkSerializeWriter writer)
+        {
+            base.Write(writer);
+            writer.Write(Persist);
+            writer.Write(MaxOut);
+        }
+    }
+}
