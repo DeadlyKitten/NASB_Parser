58549e4c67cb508ec187bf80904dcf95a84f56d0
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2022-11-13 01:20:22 -0800

    Fix HurtBone

--- NASB_Parser/StateActions/HurtBone.cs
+++ NASB_Parser/StateActions/HurtBone.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NASB_Parser.FloatSources;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,12 +8,13 @@ namespace NASB_Parser.StateActions
     public class HurtBone : ISerializable
     {
         public HurtType Type { get; set; }
-        public int Armor { get; set; }
-        public int KnockbackArmor { get; set; }
+        public FloatSource Armor { get; set; }
+        public FloatSource KnockbackArmor { get; set; }
         public bool ForceZ0 { get; set; }
+        public bool IgnoreGrab { get; set; }
         public string BoneA { get; set; }
         public string BoneB { get; set; }
-        public float Radius { get; set; }
+        public FloatSource Radius { get; set; }
         public Vector3 LocalOffsetA { get; set; }
         public Vector3 WorldOffsetA { get; set; }
         public Vector3 LocalOffsetB { get; set; }
@@ -26,28 +28,44 @@ namespace NASB_Parser.StateActions
         {
             int version = reader.ReadInt();
             Type = (HurtType)reader.ReadInt();
-            Armor = reader.ReadInt();
-            if (version > 0)
+            if (version > 1)
             {
-                KnockbackArmor = reader.ReadInt();
+                Armor = FloatSource.Read(reader);
+                KnockbackArmor = FloatSource.Read(reader);
+            }
+            else
+            {
+                Armor = new FSValue(reader.ReadFloat());
+                KnockbackArmor = new FSValue(reader.ReadFloat());
             }
             ForceZ0 = reader.ReadBool();
             BoneA = reader.ReadString();
             BoneB = reader.ReadString();
-            Radius = reader.ReadFloat();
-            LocalOffsetA = reader.ReadVector3();
-            WorldOffsetA = reader.ReadVector3();
-            LocalOffsetB = reader.ReadVector3();
-            WorldOffsetB = reader.ReadVector3();
+            if (version > 1)
+            {
+                Radius = FloatSource.Read(reader);
+                LocalOffsetA = reader.ReadVector3(true);
+                WorldOffsetA = reader.ReadVector3(true);
+                LocalOffsetB = reader.ReadVector3(true);
+                WorldOffsetB = reader.ReadVector3(true);
+            }
+            else
+            {
+                Radius = new FSValue(reader.ReadFloat());
+                LocalOffsetA = reader.ReadVector3();
+                WorldOffsetA = reader.ReadVector3();
+                LocalOffsetB = reader.ReadVector3();
+                WorldOffsetB = reader.ReadVector3();
+            }
         }
 
         public void Write(BulkSerializeWriter writer)
         {
-            writer.Write(1);
+            writer.Write(2);
             writer.Write(Type);
             writer.Write(Armor);
             writer.Write(KnockbackArmor);
-            writer.Write(0);
+            writer.Write(IgnoreGrab);
             writer.Write(BoneA);
             writer.Write(BoneB);
             writer.Write(Radius);
