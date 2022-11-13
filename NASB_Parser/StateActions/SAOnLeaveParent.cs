3c8a99e4b1f46eb10d1aa56859a1c7cb29a8e5a8
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2022-11-13 01:21:43 -0800

    Add SAOnLeaveParent

---
+++ NASB_Parser/StateActions/SAOnLeaveParent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NASB_Parser.StateActions
+{
+    public class SAOnLeaveParent : StateAction
+    {
+        public StateAction Action { get; set; }
+
+        public SAOnLeaveParent()
+        {
+        }
+
+        internal SAOnLeaveParent(BulkSerializeReader reader) : base(reader)
+        {
+            Action = Read(reader);
+        }
+
+        public override void Write(BulkSerializeWriter writer)
+        {
+            base.Write(writer);
+            writer.Write(Action);
+        }
+    }
+}
