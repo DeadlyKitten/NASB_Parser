c3245eaec6d9a2fcadd298c96e3bac04fbfd827c
DeadlyKitten <9684760+DeadlyKitten@users.noreply.github.com>
2023-07-02 20:23:26 -0700

    Fix Helper Classes.

--- NASB_Parser/Extensions.cs
+++ NASB_Parser/Extensions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NASB_Parser.FloatSources;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,16 +7,23 @@ namespace NASB_Parser
 {
     public static class Extensions
     {
-        public static Vector3 ReadVector3(this BulkSerializeReader reader)
+        public static Vector3 ReadVector3(this BulkSerializeReader reader, bool useFloatSource = false)
         {
-            return new Vector3 { x = reader.ReadFloat(), y = reader.ReadFloat(), z = reader.ReadFloat() };
+            if (useFloatSource)
+            {
+                var x = new FSValue(reader).Value;
+                var y = new FSValue(reader).Value;
+                var z = new FSValue(reader).Value;
+                return new Vector3(x, y, z);
+            }
+            else return new Vector3(reader);
         }
 
         public static void Write(this BulkSerializeWriter writer, Vector3 val)
         {
-            writer.AddFloat(val.x);
-            writer.AddFloat(val.y);
-            writer.AddFloat(val.z);
+            writer.Write(new FSValue(val.x));
+            writer.Write(new FSValue(val.y));
+            writer.Write(new FSValue(val.z));
         }
 
         public static void Write<T>(this BulkSerializeWriter writer, T t) where T : Enum
--- NASB_Parser/Vector3.cs
+++ NASB_Parser/Vector3.cs
@@ -10,5 +10,19 @@ namespace NASB_Parser
         public float x;
         public float y;
         public float z;
+
+        public Vector3(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public Vector3(BulkSerializeReader reader)
+        {
+            x = reader.ReadFloat();
+            y = reader.ReadFloat();
+            z = reader.ReadFloat();
+        }
     }
 }
